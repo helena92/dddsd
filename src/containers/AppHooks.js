00b4ec18cfe59d2f4e088947ae5b5ab993db7de8
Elena Chornobai <el@gmail.com>
2021-11-08 12:51:20 +0200

    redux

---
+++ src/containers/AppHooks.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+import './App.css';
+
+function App() {
+  // constructor() {
+  //   super()
+  //   this.state = {
+  //     robots: [],
+  //     searchfield: ''
+  //   }
+  // }
+
+  // componentDidMount() {
+  // fetch('https://jsonplaceholder.typicode.com/users')
+  // .then(response => response.json())
+  // .then(users => { setRobots(users) });
+  // }
+
+  // const { robots, searchfield } = this.state;
+
+  const [robots, setRobots] = useState([])
+  const [searchfield, setSearchfield] = useState('')
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => { setRobots(users) });
+    // console.log(count);
+  }, [count])
+
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value)
+  }
+
+  const filteredRobots = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  })
+  return !robots.length ?
+    <h1>Loading</h1> :
+    (
+      <div className='tc'>
+        <h1 className='f1'>RoboFriends</h1>
+        <button onClick={() => setCount(count + 1)}>Click me</button>
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          <ErrorBoundry>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundry>
+        </Scroll>
+      </div>
+    );
+}
+
+export default App;
