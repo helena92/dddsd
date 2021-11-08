c9aebb2908fa263452feed942eecfaf5ce1c1416
Elena Chornobai <el@gmail.com>
2021-11-08 12:34:19 +0200

    added redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
-import 'tachyons'; 
+// import registerServiceWorker from './registerServiceWorker';
+import { searchRobots, requestRobots } from './reducers'
+import { createLogger } from 'redux-logger'
+import ThunkMiddleware from 'redux-thunk';
+import 'tachyons';
 
+const logger = createLogger();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+const store = createStore(rootReducer, applyMiddleware(ThunkMiddleware, logger))
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 // registerServiceWorker();
